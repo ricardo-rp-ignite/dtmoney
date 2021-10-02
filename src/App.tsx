912b4ade6e4e18527c26ad6d87145d5384159e07
moro <ricardoromero4444@gmail.com>
2021-10-02 16:53:19 -0300

    💄 : Added global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
+import { GlobalStyle } from "./styles/global"
 
 export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank" 
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> Hello World </h1>
+      <GlobalStyle />
     </div>
-  );
+  )
 }
