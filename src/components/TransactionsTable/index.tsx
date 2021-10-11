291427f2e5c9d2d1fba89de1f99acc7ee852c580
moro <ricardoromero4444@gmail.com>
2021-10-11 14:19:24 -0300

    👽️ Renamed classnames

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -15,14 +15,14 @@ export function TransactionsTable(): React.ReactElement {
         <tbody>
           <tr>
             <td>Desenvolvimento de website</td>
-            <td className="income">R$12.000</td>
+            <td className="deposit">R$12.000</td>
             <td>Desenvolvimento</td>
             <td>20/02/2020</td>
           </tr>
 
           <tr>
             <td>Aluguel</td>
-            <td className="expenditure">- R$1.100</td>
+            <td className="withdrawal">- R$1.100</td>
             <td>Casa</td>
             <td>30/02/2020</td>
           </tr>
