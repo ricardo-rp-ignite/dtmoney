5ab4c72c170d1e2be3cd6892d8d4b8467ed8bce4
moro <ricardoromero4444@gmail.com>
2021-10-11 14:33:24 -0300

    ✨ Added mirageJs

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react'
 import { Container } from './styles'
 
 export function TransactionsTable(): React.ReactElement {
+  useEffect(() => {
+    fetch('http://localhost:3000/api/transactions')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
+
   return (
     <Container>
       <table>
