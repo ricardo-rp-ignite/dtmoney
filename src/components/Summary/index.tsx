dd8d4c402eeda2c7beaad267b4bd4d86fd1e664d
moro <ricardoromero4444@gmail.com>
2021-10-10 22:14:06 -0300

    ✨ Added summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import incomeImg from '../../assets/income.svg'
+import expenditureImg from '../../assets/expenditure.svg'
+import totalImg from '../../assets/total.svg'
+
+import { Container } from './styles'
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saidas</p>
+          <img src={expenditureImg} alt="Despesas" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+      <div className="green-background">
+        <header>
+          <p>Entradas</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  )
+}
